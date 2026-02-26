6805e2762a3f588b19282877d9915d13f8455dc3
NH-9 <noahhulett@my.whccd.edu>
2026-02-25 19:17:27 -0800

    data collection created

--- COMP003A.Assignment7/Program.cs
+++ COMP003A.Assignment7/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Precalculus Exam 1 Scores Analyzer");
+            Console.WriteLine("----------------------------------");
+
+            List<int> scores = new List<int> { 9, 20, 24, 28, 30, 30, 37, 28, 43, 50};
+            
         }
     }
 }
