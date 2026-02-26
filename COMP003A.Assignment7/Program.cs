4feeec61b580a0b802e0175c6c3ee71d22311e8a
NH <noahhulett@my.whccd.edu>
2026-02-25 23:18:51 -0800

    methods created for displaying data

--- COMP003A.Assignment7/Program.cs
+++ COMP003A.Assignment7/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace COMP003A.Assignment7
+﻿using System.Threading.Tasks.Sources;
+using System.Xml.Serialization;
+
+namespace COMP003A.Assignment7
 {
     internal class Program
     {
@@ -7,8 +10,43 @@
             Console.WriteLine("Precalculus Exam 1 Scores Analyzer");
             Console.WriteLine("----------------------------------");
 
-            List<int> scores = new List<int> { 9, 20, 24, 28, 30, 30, 37, 28, 43, 50};
+            List<double> scores = new List<double> { 9, 20, 24, 28, 30, 30, 37, 38, 43, 50};
+
+            DisplayAll(scores);
+            DisplayTotal(scores);
+            DisplayAverage(scores);
             
+        }
+
+        static void DisplayAll(List<double> scores)
+        {
+            Console.Write("Scores: ");
+            foreach (double score in scores)
+            {
+                Console.Write($"{score} ");
+            }
+            Console.WriteLine("\n");
+        }
+
+        static void DisplayTotal(List<double> scores)
+        {
+            double total = 0;
+            foreach (double score in scores)
+            {
+                total += score;
+            }
+            Console.WriteLine($"Total: {total}\n");
+        }
+
+        static void DisplayAverage(List<double> scores)
+        {
+            double average = 0;
+            foreach (double score in scores)
+            {
+                average += score;
+            }
+            average /= scores.Count;
+            Console.WriteLine($"Average: {average}\n");
         }
     }
 }
